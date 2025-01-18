690b9af362f1f38a6db36e69f9f2dc4cfc2fcfe3
TimaSv-Uk <sviridenkotimofei@gmail.com>
2025-01-18 20:25:01 +0200

    118 leetcode simple problem in 30 min

--- LeetCode/Easy/108.php
+++ LeetCode/Easy/108.php
@@ -22,37 +22,34 @@ class TreeNode
 		$this->right = $right;
 	}
 }
-class Solution
+
+/**
+ * @param Integer[] $nums
+ * @return TreeNode
+ */
+function sortedArrayToBST($nums)
 {
 
-	/**
-	 * @param Integer[] $nums
-	 * @return TreeNode
-	 */
-	function sortedArrayToBST($nums)
-	{
-
-		if ($nums == null) {
-			return new TreeNode();
-		}
-
-		$middle_index = floor(count($nums) / 2);
-
-		$left_side = null;
-		$right_side = null;
-
-		if ($middle_index - 1 >= 0) {
-
-			$left_side = $this->sortedArrayToBST(array_slice($nums, 0, $middle_index));
-		};
-		if ($middle_index + 1 < count($nums)) {
-
-			$right_side = $this->sortedArrayToBST(array_slice($nums, $middle_index + 1));
-		};
-
-
-
-		return new TreeNode($nums[$middle_index], $left_side, $right_side);;
+	if ($nums == null) {
+		return new TreeNode();
 	}
+
+	$middle_index = floor(count($nums) / 2);
+
+	$left_side = null;
+	$right_side = null;
+
+	if ($middle_index - 1 >= 0) {
+
+		$left_side = sortedArrayToBST(array_slice($nums, 0, $middle_index));
+	};
+	if ($middle_index + 1 < count($nums)) {
+
+		$right_side =sortedArrayToBST(array_slice($nums, $middle_index + 1));
+	};
+
+
+
+	return new TreeNode($nums[$middle_index], $left_side, $right_side);;
 }
-var_dump((new Solution())->sortedArrayToBST([-1, 0, 1, 2]));
+var_dump(sortedArrayToBST([-1, 0, 1, 2]));
---
+++ LeetCode/Easy/118.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace LeetCode\Easy;
+
+use function Symfony\Component\VarDumper\Dumper\esc;
+
+/**
+ * @param Integer $numRows
+ * @return Integer[][]
+ */
+function generate($numRows)
+{
+	if ($numRows === 0) {
+		return [];
+	}
+	if ($numRows === 1) {
+		return [[1]];
+	}
+	$pascal_triangle_rows = [[1], [1, 1]];
+	$current_row = [];
+
+	for ($i = 2; $i < $numRows; $i++) {
+
+		$previos_row = $pascal_triangle_rows[$i - 1];
+
+		$current_row[] = 1;
+		for ($j = 0; $j < count($previos_row) - 1; $j++) {
+
+			$current_row[] = $previos_row[$j] + $previos_row[$j + 1];
+		}
+
+		$current_row[] = 1;
+
+		$pascal_triangle_rows[] = $current_row;
+		// print_r($pascal_triangle_rows);
+		$current_row  = [];
+	}
+	return $pascal_triangle_rows;
+}
+
+function next_row(array $array)
+{
+	$new_array = [];
+	for ($i = 0; $i < count($array) - 1; $i++) {
+
+		$new_array[] = $array[$i] + $array[$i + 1];
+	}
+	$new_array;
+	return $new_array;
+};
+// print_r(next_row([1, 3, 3, 1]));
+// generate(5);
+print_r(
+	generate(5)
+);
